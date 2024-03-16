74472ed697ecffe1eeb8bcb25c5ce1196d178a3b
이효준 <102369002+HP657@users.noreply.github.com>
2024-03-16 19:56:26 +0900

    Create 14215.cpp

---
+++ cpp/bronze/14215.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int arr[3];
+    for (int i = 0; i < 3; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr+3);
+    
+    while (true) {
+        if ((arr[0] + arr[1]) > arr[2]) {
+            break;
+        }
+        arr[2]--;
+    }
+    cout << arr[0] + arr[1] + arr[2];
+    return 0;
+}
