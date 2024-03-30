a83eb0cc3f074e81e3eef9ce681d769688d58639
HP657 <junlee5070@gmail.com>
2024-03-30 18:24:22 +0900

    10828번 문제

---
+++ cpp/sliver/10828.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int st[10000];
+    int cur = 0;
+    for (int i = 0; i < n; i++) {
+        string m;
+        int temp;
+        cin >> m;
+        if (m == "push") {
+            cin >> temp;
+            st[cur] = temp;
+            cur++;
+        }
+        else if (m == "pop") {
+            if (cur > 0) {
+                cout << st[cur - 1];
+                cur--;
+            } else {
+                cout << -1;
+            }
+            cout << '\n';
+        }
+        else if (m == "size") {
+            cout << cur;
+            cout << '\n';
+        }
+        else if (m == "empty") {
+            if (cur == 0) {
+                cout << 1;
+            } else {
+                cout << 0;
+            }
+            cout << '\n';
+        }
+        else if (m == "top") {
+            if (cur > 0) {
+                cout << st[cur - 1];
+            } else {
+                cout << -1;
+            }
+            cout << '\n';
+        }
+    }
+    return 0;
+}
