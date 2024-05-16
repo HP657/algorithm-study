2712eb99477dd9c32a31b53d33f95ecd25686fe9
HP657 <junlee5070@gmail.com>
2024-05-16 23:04:04 +0900

    2178번 문제 bfs 문제 구조에 대해서 좀 더 알아봐야 할듯

--- cpp/sliver/2178.cpp
+++ cpp/sliver/2178.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
-#include <vector>
+#include <queue>
+#define MAX 101
 using namespace std;
+
+
+int n, m;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int maze[MAX][MAX]; // 미로 표현
+int visited[MAX][MAX]; // 방문 기록
+int dist[MAX][MAX]; // 이동한 칸 기록
+
+queue<pair<int, int>> q;
+
+void bfs(int start_x, int start_y) {
+    visited[start_x][start_y] = 1;
+    q.push(make_pair(start_x, start_y));
+    dist[start_x][start_y]++;
+
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int new_x = x + dx[i];
+            int new_y = y + dy[i];
+
+            bool ify = ((new_x >= 0 && new_x < n) && (new_y >= 0 && new_y < m) && !visited[new_x][new_y] && maze[new_x][new_y] == 1);
+            if (ify) {
+                visited[new_x][new_y] = 1;
+                q.push(make_pair(new_x, new_y));
+                dist[new_x][new_y] = dist[x][y] + 1;
+            }
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m;
     cin >> n >> m;
-    
-    int dx[4] = {1, 0, -1, 0};
-    int dy[4] = {0, 1, 0, -1};
-
-    int arr[n][m];
 
     string input;
     for (int i = 0; i < n; i++) {
         cin >> input; 
         for (int j = 0; j < m; j++) {
-            arr[i][j] = input[j] - '0';
+            maze[i][j] = input[j] - '0';
         }
     }
 
+    bfs(0, 0);
 
-
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << arr[i][j] << " ";  
-        }
-        cout << '\n';  
-    }
+    cout << dist[n - 1][m - 1];
     return 0;
 }
-// https://wooono.tistory.com/410
