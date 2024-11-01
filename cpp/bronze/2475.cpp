56ca6116de64655477b379610a5395eedcc84ccf
HP657 <junlee5070@gmail.com>
2024-11-01 21:12:07 +0900

    2475번 문제

---
+++ cpp/bronze/2475.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int temp, sum = 0;
+    for (int i = 0; i < 5; i++) {
+        cin >> temp;
+        sum += temp * temp;
+    }
+
+    cout << sum % 10;
+
+    return 0;
+}
