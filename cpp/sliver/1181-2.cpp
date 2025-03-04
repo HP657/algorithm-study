cfa6ef6d4ed7a374ae70bbd15736179d5c34f093
HP657 <junlee5070@gmail.com>
2025-03-05 00:20:12 +0900

    answer벡터 따로 생성 1181번 문제

---
+++ cpp/sliver/1181-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool sol(string a, string b) {
+	if (a.length() == b.length()) {
+		return a < b;
+	}
+	else {
+		return a.length() < b.length();
+	}
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<string> word(N);
+    vector<string> answer;
+    for (int i = 0; i < N; i++) {
+        cin >> word[i];
+    }
+
+    sort(word.begin(), word.end(), sol);
+
+    answer.push_back(word[0]);
+    for (int i = 1; i < N; i++) {
+        if (word[i] != word[i - 1]) {
+            answer.push_back(word[i]);
+        }
+    }
+
+    for (auto a : answer) {
+        cout << a << '\n';
+    }
+    return 0;
+}
