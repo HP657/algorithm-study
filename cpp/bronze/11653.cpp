006d575a62fde188226a779b0a7e77b2b2f01a88
HP657 <junlee5070@gmail.com>
2025-08-23 08:22:19 +0900

    cpp/bronze/11653번 풀이

---
+++ cpp/bronze/11653.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    while (n != 1) {
+        for (int i = 2; i < n + 1; i++ ) {
+            if (n % i == 0) {
+                cout << i << '\n';
+                n /= i;
+                break;
+            }
+        }
+    }
+    return 0;
+}
